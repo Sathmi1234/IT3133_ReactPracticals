3c8d27ddb84de7dbe0d33207d2004b268b8e64aa
Sathmi Rajapaksha <139479328+Sathmi1234@users.noreply.github.com>
2024-11-26 08:34:02 +0000

    Added Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { useState } from "react";
-import ChildCom from "./componenets/Day7_19_11/ChildCom";
+import { BrowserRouter as Router, Routes,Route, Link, NavLink } from "react-router-dom";
+import Home from "./componenets/Day8_26_11/Home";
+import AboutUs from "./componenets/Day8_26_11/AboutUs";
+import ContactUs from "./componenets/Day8_26_11/ContactUs";
 
 function App() {
-  const [childname,setChildname]=useState("Default");
-  const recieveName=(name)=>{
-    setChildname(name)
-  }
   return (
-    <div>
-      <h2>Hello React I'm Parent</h2>
-      <ChildCom setfun={recieveName}/>
-      <p>Parent: My Child said "{childname}"</p>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/about-us" element={<AboutUs/>}/>
+          <Route path="/contact-us" element={<ContactUs/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
