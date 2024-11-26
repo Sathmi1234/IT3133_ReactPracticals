73887897e4b5eaf94df23b3fb4e3a198937cbaf7
Sathmi Rajapaksha <139479328+Sathmi1234@users.noreply.github.com>
2024-11-26 09:53:11 +0000

    Created simple login page

--- src/componenets/Day8_26_11/Ex2/Login.js
+++ src/componenets/Day8_26_11/Ex2/Login.js
@@ -1,14 +1,51 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Login(){
     const navigate=useNavigate();
-    const authenticate=()=>{
-        navigate('/dash')
+    const [user,setUser] = useState({
+        username:'',
+        password:''
+    });
+    
+    const getUser=(e)=>{
+        const { name, value } = e.target;
+        setUser((prev) => ({
+                ...prev,
+                [name]: value
+            })
+        );
     }
+    const authenticate=()=>{
+        if (user.username==='admin' && user.password==='admin1234') {
+            navigate('/dash')
+        } else {
+            alert('Incorrect Username or Password');
+        }        
+    }
+    
     return(
         <div>
             <h1>Login Here</h1>
-            <button onClick={authenticate}>Login</button>
+            <form>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Username</td>
+                            <td><input type="text" name="username" value={user.username} onChange={getUser}/></td>
+                        </tr>
+                        <tr>
+                            <td>Password</td>
+                            <td><input type="password" name="password" value={user.password} onChange={getUser}/></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><button onClick={authenticate}>Login</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+            
         </div>
     )
 }
