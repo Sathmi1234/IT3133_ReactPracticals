fbb7572b8a558fec5611bc09e9523d8ede336957
Sathmi Rajapaksha <139479328+Sathmi1234@users.noreply.github.com>
2024-11-26 08:43:07 +0000

    Home,About Us, Contact Us linked to the App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import { BrowserRouter as Router, Routes,Route, Link, NavLink } from "react-rout
 import Home from "./componenets/Day8_26_11/Home";
 import AboutUs from "./componenets/Day8_26_11/AboutUs";
 import ContactUs from "./componenets/Day8_26_11/ContactUs";
+import { Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <nav>
+          <ul>
+            <li><Link to='/home'>Home</Link></li>
+            <li><Link to='/about-us'>About Us</Link></li>
+            <li><Link to='/contact-us'>Contact Us</Link></li>
+          </ul>
+        </nav>
         <Routes>
           <Route path="/home" element={<Home/>}/>
           <Route path="/about-us" element={<AboutUs/>}/>
           <Route path="/contact-us" element={<ContactUs/>}/>
         </Routes>
       </Router>
+
     </div>
   );
 }
