64bc72a019cd233908a01326b1df8d33daff3d41
Sathmi Rajapaksha <139479328+Sathmi1234@users.noreply.github.com>
2024-11-26 09:06:05 +0000

    Login Page and Dashboard created for Exercise 2

---
+++ src/componenets/Day8_26_11/Ex2/Login.js
@@ -0,0 +1,10 @@
+import { Button } from "bootstrap";
+
+export default function Login(){
+    return(
+        <div>
+            <h1>Login Here</h1>
+            <Button>Login</Button>
+        </div>
+    )
+}
