ef8f81ed34407bed1c8136a7bd97e70d925fe79e
Sathmi Rajapaksha <139479328+Sathmi1234@users.noreply.github.com>
2024-11-26 08:23:15 +0000

    About Us, Contact Us created and Home updated

---
+++ src/componenets/Day8_26_11/ContactUs.js
@@ -0,0 +1,7 @@
+export default function ContactUs(){
+    return(
+        <div>
+            <h1>Welcome!! this is Contact Us Page</h1>
+        </div>
+    )
+}
--- src/componenets/Day8_26_11/Home.js
+++ src/componenets/Day8_26_11/Home.js
@@ -1,7 +1,7 @@
 export default function Home(){
     return(
         <div>
-
+            <h1>Welcome!! this is Home Page</h1>
         </div>
     )
 }
