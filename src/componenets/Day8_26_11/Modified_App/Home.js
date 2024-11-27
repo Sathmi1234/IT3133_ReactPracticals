2ff2619eec4236eb93a78cdd966217a4998de9f1
Sathmi1234 <devsadini@gmail.com>
2024-11-27 18:35:01 +0530

    Added Login button to home and created navbar component

--- src/componenets/Day8_26_11/Modified_App/Home.js
+++ src/componenets/Day8_26_11/Modified_App/Home.js
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home(){
+    const navigate=useNavigate();
+    const navigateTo=()=>{
+        navigate('/login')
+    }
     return(
         <div>
             <h1>Welcome!! this is Home Page</h1>
+            <button onClick={navigateTo}>Login</button>
         </div>
     )
 }
---
+++ src/componenets/Day8_26_11/Modified_App/NavBar.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Link } from "react-router-dom";
+export default function NavBar(){
+    return(
+        <div>
+            <nav className="navbar">
+                <ul className="navbar-list">
+                    <li className="navbar-item">
+                    <Link to="/" className="navbar-link">Home</Link>
+                    </li>
+                    <li className="navbar-item">
+                    <Link to="/about-us" className="navbar-link">About Us</Link>
+                    </li>
+                    <li className="navbar-item">
+                    <Link to="/contact-us" className="navbar-link">Contact Us</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
