44724467dcbd5eb0a04eeec7379d24e69faccab8
Sathmi1234 <devsadini@gmail.com>
2024-12-03 23:13:34 +0530

    App.js completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./componenets/Day8_26_11/Modified_App/Home";
-import AboutUs from "./componenets/Day8_26_11/Modified_App/AboutUs";
-import ContactUs from "./componenets/Day8_26_11/Modified_App/ContactUs";
-import Login from "./componenets/Day8_26_11/Modified_App/Login";
-import Dashboard from "./componenets/Day8_26_11/Modified_App/Dashboard";
-import './App.css';
-import NavBar from "./componenets/Day8_26_11/Modified_App/NavBar";
+import React from 'react';
+import './componenets/ICAE01/assets/CSS/layout.css';
+import Products from './componenets/ICAE01/components/Products';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dash" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="main-grid-container">
+            <Products />
+        </div>
+    );
 }
 
 export default App;
