34c0772d265d929dbf4257aad6fba65dbc7fed74
Sathmi1234 <devsadini@gmail.com>
2024-12-03 22:14:08 +0530

    Created Product component to display flower details

--- src/componenets/ICAE01/components/Product.js
+++ src/componenets/ICAE01/components/Product.js
@@ -1,20 +1,25 @@
+import { flowers } from '../components/FlowerDB';
 import '../assets/CSS/layout.css';
-export default function Product(){
-    
-    return(
-        <div className="grid-item">
 
-            <div class="card">
-                <img  />
-                <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
-                    <div class="quantity-container">
-                        <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
-                    </div>
-                    <button class="card-button">Add to Cart</button>
-                </div>
+export default function Product() {
+  return (
+    <div className="grid-container">
+      {flowers.map((flower) => (
+        <div className="grid-item" key={flower.id}>
+          <div className="card">
+            <img src={require(`../assets/image/${flower.img}`)} alt={flower.name} />
+            <div className="card-body">
+              <h5 className="card-title">{flower.name}</h5>
+              <h5 className="card-title">Price: ${flower.price}</h5>
+              <div className="quantity-container">
+                <label htmlFor={`quantity-${flower.id}`}>Quantity:</label>
+                <input type="number" id={`quantity-${flower.id}`} name="quantity" />
+              </div>
+              <button className="card-button">Add to Cart</button>
             </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
